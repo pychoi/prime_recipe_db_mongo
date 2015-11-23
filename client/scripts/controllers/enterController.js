5be366dffcaeee671cc61a91cde44b9cf87f7f51
mmpyhmm <choipui@gmail.com>
2015-11-23 01:04:46 -0600

    accordion for enter recipe

--- client/scripts/controllers/enterController.js
+++ client/scripts/controllers/enterController.js
@@ -1,22 +1,45 @@
 myApp.controller('EnterController', ['$scope', '$http', function($scope, $http){
     console.log("EnterController is working!");
 
-    $scope.recipe = {};
+    $scope.recipeInfo = {};
     $scope.ingredient = {};
     $scope.ingredients = [];
     $scope.categories = [];
+    $scope.disable = false;
 
     // $scope.steps array contains all step numbers and corresponding instructions as objects
     $scope.steps = [{step: 'Step 1'}];
 
-    $scope.addNewStep = function(){
-        var newItemNum = $scope.steps.length + 1;
-        $scope.steps.push({step: 'Step '+ newItemNum});
-        //console.log($scope.steps);
+    $scope.main = ['Beef', 'Pork', 'Chicken/Poultry', 'Lamb/Goat', 'Seafood', 'Pasta/Noodle', 'Tofu', 'Vegetables', 'Egg', 'Rice/Congee'];
+    $scope.cuisine =['Chinese', 'American', 'Italian', 'Mexican', 'Japanese', 'Korean', 'Indian', 'Southeast Asian (Thai, Vietnamese, Singaporean, Malaysian)'];
+    $scope.type = ['Bread', 'Dessert', 'Dim Sum', 'Soup', 'Chinese Festival', 'BBQ', 'Herbal Medicine', 'Salad', 'Sauces'];
+
+
+    $scope.addName = function(recipeInfo){
+        //$scope.accordion = 2;
+        $http.post('/submit/recipeInfo', $scope.recipeInfo).then(function(){
+            console.log("Recipe Info saved");
+            $scope.disable = true;
+        });
     };
+
+    $scope.deleteName = function(){
+        $scope.disable = false;
+        $scope.recipeInfo = {};
+        $http.delete('/submit/recipeInfo').then(function(){
+            console.log('deleted recipe info!');
+        });
+    };
+
 
     $scope.addNewIngredient = function(){
         $scope.ingredients.push($scope.ingredient);
+
+        $http.post('/submit/ingredient', $scope.ingredient).then(function(response){
+            console.log(response);
+            console.log("Ingredient saved");
+        });
+
         $scope.ingredient = {};
     };
 
@@ -26,24 +49,36 @@ myApp.controller('EnterController', ['$scope', '$http', function($scope, $http){
                 $scope.ingredients.splice(i,1);
             }
         }
-        //console.log($scope.ingredients);
+
+        $http.delete('/submit/ingredient').then(function(){
+            console.log('deleted ingredient!');
+        });
+
+    };
+
+    $scope.addNewStep = function(){
+        var newItemNum = $scope.steps.length + 1;
+        $scope.steps.push({step: 'Step '+ newItemNum});
+        //console.log($scope.steps);
+    };
+
+    //remove step function...
+
+    $scope.toggleChecked = function(foo){
+        if ($scope.categories.indexOf(foo) === -1){
+            $scope.categories.push(foo);
+        } else {
+            $scope.categories.splice($scope.categories.indexOf(foo), 1);
+        }
     };
 
     $scope.submitRecipe = function(){
-        console.log($scope.recipe);
-        //console.log($scope.ingredients);
-        //console.log($scope.steps);
-        $http.post('/submit', $scope.recipe).then(function(){
-            $scope.getRecipeId();
+        $http.post('/submit/categories', $scope.categories).then(function(){
+            console.log("success");
         });
+
+        //check if there's at least on ingredient and amount, and at least one step
 
     };
-
-    $scope.getRecipeId = function(){
-        $http.get('/submit').then(function(){
-
-        });
-    }
-
 
 }]);
--- server/routes/submit.js
+++ server/routes/submit.js
@@ -4,15 +4,18 @@ var path = require('path');
 var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/graces_kitchen';
 
-router.post("/", function(req, res, next){
+router.post("/recipeInfo", function(req, res, next){
+
+    console.log(req.body);
+
     var recipeInfo = {
-        "recipe_name": req.body.name,
-        "recipe_source":req.body.source
+        "name": req.body.name,
+        "source":req.body.source
     };
 
     pg.connect(connectionString, function(err, client){
         client.query("INSERT INTO recipes (recipe_name, recipe_source) VALUES ($1, $2)",
-            [recipeInfo.recipe_name, recipeInfo.recipe_source],
+            [recipeInfo.name, recipeInfo.source],
             function (err, result) {
                 if (err) {
                     console.log("Error inserting data: ", err);
@@ -22,5 +25,41 @@ router.post("/", function(req, res, next){
             });
     });
 });
+
+router.delete('/recipeInfo', function(req,res,next){
+    pg.connect(connectionString, function(err, client){
+        client.query("DELETE FROM recipes WHERE id = (SELECT id FROM recipes ORDER BY id DESC LIMIT 1)",
+            function (err, result) {
+                if (err) {
+                    console.log("Error inserting data: ", err);
+                    res.send(false);
+                }
+                res.send(true);
+            });
+    });
+});
+
+router.post("/ingredient", function(req, res, next){
+
+    console.log(req.body);
+
+    var ingredient = {
+        "name": req.body.name,
+        "amount":req.body.amount
+    };
+
+    pg.connect(connectionString, function(err, client){
+        client.query("INSERT INTO ingredients (ingredient_name, ingredient_amount, recipe_id) VALUES ($1, $2, (SELECT id FROM recipes ORDER BY id DESC LIMIT 1))",
+            [ingredient.name, ingredient.amount],
+            function (err, result) {
+                if (err) {
+                    console.log("Error inserting data: ", err);
+                    res.send(false);
+                }
+                res.send(true);
+            });
+    });
+});
+
 
 module.exports = router;
