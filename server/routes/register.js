42537a4603dd8cd0461e9e76ef78c0a07607e3d1
mmpyhmm <choipui@gmail.com>
2015-12-03 09:57:31 -0600

    add page for registration error

--- server/routes/register.js
+++ server/routes/register.js
@@ -12,10 +12,15 @@ router.post('/', function(req, res, next){
     Users.create(req.body, function(err, post){ //.create is mongoose
         if(err){
             next(err);
+            res.redirect('/register/failed')
         } else {
             res.redirect('/');
         }
     });
+});
+
+router.get('/failed', function(req,res,next){  //send to the register page
+    res.sendFile(path.resolve(__dirname, '../public/assets/views/registerfailed.html'));
 });
 
 module.exports = router;
